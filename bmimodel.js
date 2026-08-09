2fea9b7d3bb4ba517e79e0aa46d75c475e9f7b55
myteamaccess234-lang <myteamaccess234@gmail.com>
2026-08-09 17:02:39 +0530

    Update bmimodel.js

--- bmimodel.js
+++ bmimodel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const bmiSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     email: { type: String, required: true },
     age: { type: Number, required: true },
     height: { type: Number, required: true },
@@ -8,6 +9,6 @@ const bmiSchema = new mongoose.Schema({
     bmi: { type: Number, required: true },
     category: { type: String, required: true },
     date: { type: String, required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('BmiRecord', bmiSchema);
