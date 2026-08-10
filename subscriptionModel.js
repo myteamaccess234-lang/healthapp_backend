98e56bc06ed044b068bf3fd5af57a1508e267fd5
myteamaccess234-lang <myteamaccess234@gmail.com>
2026-08-10 16:58:58 +0530

    Update subscriptionModel.js

--- subscriptionModel.js
+++ subscriptionModel.js
@@ -4,18 +4,21 @@ const subscriptionSchema = new mongoose.Schema({
   userId: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'User', 
-    required: false,
-    index: true // Speeds up Subscription.find({ userId }) lookups
+    required: true,
+    index: true // Speeds up Subscription.find({ userId }) queries
   },
   endpoint: { 
     type: String, 
     required: true, 
-    unique: true 
+    unique: true,
+    index: true 
   },
   keys: {
     p256dh: { type: String, required: true },
     auth: { type: String, required: true }
   }
-}, { timestamps: true });
+}, { 
+  timestamps: true 
+});
 
 module.exports = mongoose.model('Subscription', subscriptionSchema);
