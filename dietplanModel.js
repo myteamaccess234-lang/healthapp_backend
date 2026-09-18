6e349d2a5256438ed26378e943ab7b0e79137542
myteamaccess234-lang <myteamaccess234@gmail.com>
2026-09-18 14:38:32 +0530

    Update dietplanModel.js

--- dietplanModel.js
+++ dietplanModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const dietPlanSchema = new mongoose.Schema({
-  age: { type: Number, required: true },
-  category: { type: String, required: true },
-  variant: { type: Number, required: true },
-  schedule: { type: Object, required: true }
+  age: { type: Number, required: true, unique: true },
+  categories: {
+    underweight: { type: mongoose.Schema.Types.Mixed, required: true },
+    normal: { type: mongoose.Schema.Types.Mixed, required: true },
+    overweight: { type: mongoose.Schema.Types.Mixed, required: true }
+  }
 });
 
 module.exports = mongoose.model('DietPlan', dietPlanSchema);
